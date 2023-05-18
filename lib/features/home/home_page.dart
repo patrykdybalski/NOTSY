5083b5ba72b8c5ce9847d24e80c50c36f2526990
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-05-19 00:43:42 +0200

    Implement signOut

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +8,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('niezalogowany'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'niezalogowany',
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: const Text(
+              'Wyloguj',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
