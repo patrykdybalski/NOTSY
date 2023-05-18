85b95ad7c2d3c4bb6d9812b65b3564d0f6ef71af
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-05-18 09:57:10 +0200

    Add basic styling to login page

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     super.key,
   });
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('niezalogowany'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Zaloguj się'),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'E-mail',
+              ),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: 'Hasło',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Zaloguj się'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
