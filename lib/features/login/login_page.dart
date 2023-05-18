3a960fcd73baf773ec02d08cd9391bbf357b3e21
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-05-19 00:23:15 +0200

    Add registration with bool isCreatingAccount

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -14,30 +14,41 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   var errorMessage = '';
+  var isCreatingAccount = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Zaloguj się'),
-            TextField(
-              controller: widget.emailController,
-              decoration: const InputDecoration(
-                hintText: 'E-mail',
-              ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(isCreatingAccount == true ? 'Zarejestruj się' : 'Zaloguj się'),
+          TextField(
+            controller: widget.emailController,
+            decoration: const InputDecoration(
+              hintText: 'E-mail',
             ),
-            TextField(
-              controller: widget.passwordController,
-              decoration: const InputDecoration(
-                hintText: 'Hasło',
-              ),
+          ),
+          TextField(
+            controller: widget.passwordController,
+            decoration: const InputDecoration(
+              hintText: 'Hasło',
             ),
-            Text(errorMessage),
-            ElevatedButton(
-              onPressed: () async {
+          ),
+          Text(errorMessage),
+          ElevatedButton(
+            onPressed: () async {
+              if (isCreatingAccount == true) {
+                try {
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: widget.emailController.text,
+                    password: widget.passwordController.text,
+                  );
+                } catch (error) {
+                  setState(() {
+                    errorMessage = error.toString();
+                  });
+                }
+              } else {
                 try {
                   await FirebaseAuth.instance.signInWithEmailAndPassword(
                     email: widget.emailController.text,
@@ -48,11 +59,36 @@ class _LoginPageState extends State<LoginPage> {
                     errorMessage = error.toString();
                   });
                 }
+              }
+            },
+            child: Text(
+                isCreatingAccount == true ? 'Zarejestruj się' : 'Zaloguj się '),
+          ),
+          if (isCreatingAccount == false) ...[
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  isCreatingAccount = true;
+                });
               },
-              child: const Text('Zaloguj się'),
+              child: const Text(
+                'Utwórz konto',
+              ),
             ),
           ],
-        ),
+          if (isCreatingAccount == true) ...[
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  isCreatingAccount = false;
+                });
+              },
+              child: const Text(
+                'Masz już konto?',
+              ),
+            ),
+          ]
+        ]),
       ),
     );
   }
