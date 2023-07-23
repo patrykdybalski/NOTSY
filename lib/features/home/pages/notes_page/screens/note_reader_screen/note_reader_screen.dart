b9574420025fc92da2dda58c2ea529924032e7c9
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-07-23 21:22:55 +0200

    implement RichText in appBar and add textinfo data

--- lib/features/home/pages/notes_page/screens/note_reader_screen/note_reader_screen.dart
+++ lib/features/home/pages/notes_page/screens/note_reader_screen/note_reader_screen.dart
@@ -17,33 +17,61 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       appBar: AppBar(
-          backgroundColor: AppColors.primaryColor,
-          elevation: 0.4,
-          shadowColor: Colors.black,
-          title: FittedBox(
-            fit: BoxFit.fitHeight,
-            child: Text(
-                'To test tego czy an appbarze pojawią się doskolane dopasowane słowa'),
-          )),
+        backgroundColor: AppColors.primaryColor,
+        elevation: 0.6,
+        shadowColor: Colors.black,
+        title: Expanded(
+          child: RichText(
+            text: TextSpan(
+              text: widget.noteModel.title.toString(),
+              style: const TextStyle(
+                color: AppColors.accentColor,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.only(
+          left: 15.0,
+          right: 10,
+          top: 8,
+          bottom: 10,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              widget.noteModel.title,
-              style: const TextStyle(
-                color: AppColors.accentColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  children: const [
+                    Text(
+                      'Utworzenie  23/07/2023',
+                      style: TextStyle(
+                        color: AppColors.secondaryColor,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 9,
+                      ),
+                    ),
+                    Text(
+                      'Aktualizacja 23/07/2023',
+                      style: TextStyle(
+                        color: AppColors.secondaryColor,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 9,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
             const SizedBox(
               height: 30,
             ),
             Text(
               widget.noteModel.subtitle,
-              overflow: TextOverflow.fade,
               style: const TextStyle(
                 color: AppColors.accentColor,
                 fontWeight: FontWeight.w300,
