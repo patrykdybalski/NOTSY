e3724a10f04aadfcdcac374d266abed6054da90c
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-10-24 20:53:47 +0200

    Add .fromJson in WeatherModel

--- lib/domain/models/weather_model/weather_model.dart
+++ lib/domain/models/weather_model/weather_model.dart
@@ -21,4 +21,16 @@ class WeatherModel {
   final double barometr;
   final double humidity;
   final String text;
+
+  WeatherModel.fromJson(Map<String, dynamic> json)
+      : name = json['location']['name'] + 0.0,
+        country = json['location']['country'],
+        temperature = json['current']['temp_c'] + 0.0,
+        cloudy = json['current']['cloud'] + 0.0,
+        direction = json['current']['wind_dir'],
+        wind = json['current']['wind_mph'] + 0.0,
+        rain = json['current']['precip_mm'] + 0.0,
+        barometr = json['current']['pressure_mb'] + 0.0,
+        humidity = json['current']['humidity'] + 0.0,
+        text = json['current']['condition']['text'];
 }
