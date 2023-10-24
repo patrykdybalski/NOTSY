d3e158726d3d359f4bc71be024326c8e24a5e7d9
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-10-24 21:03:46 +0200

    Add DioException in Repo

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -4,9 +4,12 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    //
-    final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=9518138900ee4740ad9193115230506&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=9518138900ee4740ad9193115230506&q=$city&aqi=no');
+      return response.data;
+    } on DioException catch (error) {
+      throw Exception(error.response?.data ?? 'Unknwon error');
+    }
   }
 }
