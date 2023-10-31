ac53588a9382588e838f2b4f603f7aa4a7db31f8
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-10-31 21:20:33 +0100

    Change userPage image and  add SingleScrollView

--- lib/app/features/home/pages/user_page/user_page.dart
+++ lib/app/features/home/pages/user_page/user_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:primary_school/app/cubit/root_cubit.dart';
-import 'package:primary_school/app/features/home/pages/user_page/cubit/user_page_cubit.dart';
 import 'package:primary_school/constans/colors.dart';
 import 'package:primary_school/constans/font_style.dart';
 
@@ -16,75 +15,68 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UserPageCubit(),
-      child: BlocBuilder<UserPageCubit, UserPageState>(
-        builder: (context, state) {
-          return Scaffold(
-            backgroundColor: AppColors.primaryColor,
-            appBar: AppBar(
-              title: const Text('Mój profil'),
-              titleTextStyle: TextStyles.headingStyle,
-              centerTitle: true,
-              backgroundColor: AppColors.primaryColor,
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+        title: const Text('Mój profil'),
+        titleTextStyle: TextStyles.headingStyle,
+        centerTitle: true,
+        backgroundColor: AppColors.primaryColor,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 16,
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  const CircleAvatar(
-                    backgroundImage: AssetImage('images/login_page_logo.png'),
-                    radius: 80,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<RootCubit>().signOut();
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: AppColors.primaryColor,
-                    ),
-                    child: Text('Wyloguj',
-                        style: GoogleFonts.domine(
-                          color: AppColors.redColor2,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w800,
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  ListTile(
-                    onTap: () {},
-                    leading: const Text('Edytuj profil'),
-                    leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
-                    trailing: const Icon(Icons.navigate_next_outlined),
-                  ),
-                  ListTile(
-                    onTap: () {},
-                    leading: const Text("Zmiana hasła"),
-                    leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
-                    trailing: const Icon(Icons.navigate_next_outlined),
-                  ),
-                  ListTile(
-                    onTap: () {},
-                    leading: const Text("Polityka prywatności"),
-                    leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
-                    trailing: const Icon(Icons.navigate_next_outlined),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                ],
+            const CircleAvatar(
+              backgroundImage: AssetImage('images/last_check_logo1.png'),
+              radius: 115,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.read<RootCubit>().signOut();
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: AppColors.greenLogoColor,
               ),
+              child: Text('Wyloguj',
+                  style: GoogleFonts.domine(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  )),
             ),
-          );
-        },
+            const SizedBox(
+              height: 40,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Text('Edytuj profil'),
+              leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
+              trailing: const Icon(Icons.navigate_next_outlined),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Text("Zmiana hasła"),
+              leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
+              trailing: const Icon(Icons.navigate_next_outlined),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: const Text("Polityka prywatności"),
+              leadingAndTrailingTextStyle: TextStyles.addEventStyle2,
+              trailing: const Icon(Icons.navigate_next_outlined),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
       ),
     );
   }
