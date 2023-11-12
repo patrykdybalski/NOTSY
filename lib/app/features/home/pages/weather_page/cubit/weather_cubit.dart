26afd8c75d902ba590bb7119fe72793821e7cd6d
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-11-12 20:14:12 +0100

    Add freezed in WeatherState

--- lib/app/features/home/pages/weather_page/cubit/weather_cubit.dart
+++ lib/app/features/home/pages/weather_page/cubit/weather_cubit.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:primary_school/app/core/enums.dart';
 import 'package:primary_school/domain/models/weather_model/weather_model.dart';
 import 'package:primary_school/domain/repositories/weather/weather_repository.dart';
-
+part 'weather_cubit.freezed.dart';
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit(this._weatherRepository) : super(const WeatherState());
+  WeatherCubit(this._weatherRepository) : super( WeatherState());
 
   final WeatherRepository _weatherRepository;
 
   Future<void> getWeatherModel({
     required String city,
   }) async {
-    emit(const WeatherState(status: Status.loading));
+    emit( WeatherState(status: Status.loading));
     try {
       final weatherModel = await _weatherRepository.getWeatherModel(city: city);
       emit(
--- lib/app/features/home/pages/weather_page/cubit/weather_state.dart
+++ lib/app/features/home/pages/weather_page/cubit/weather_state.dart
@@ -1,13 +1,23 @@
 part of 'weather_cubit.dart';
 
-@immutable
-class WeatherState {
-  const WeatherState({
-    this.model,
-    this.status = Status.initial,
-    this.errorMessage,
-  });
-  final WeatherModel? model;
-  final Status status;
-  final String? errorMessage;
+@freezed
+class WeatherState with _$WeatherState {
+  factory WeatherState({
+    @Default(null) WeatherModel? model,
+    @Default(Status.initial) Status status,
+    String? errorMessage,
+  }) = _WeatherState;
 }
+
+
+// @immutable
+// class WeatherState {
+//   const WeatherState({
+//     this.model,
+//     this.status = Status.initial,
+//     this.errorMessage,
+//   });
+//   final WeatherModel? model;
+//   final Status status;
+//   final String? errorMessage;
+// }
