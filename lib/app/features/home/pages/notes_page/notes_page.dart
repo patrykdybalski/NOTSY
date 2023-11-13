3fc719d0844c85fc3e2a1ef03154e2f5adaeb26e
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-11-13 20:41:01 +0100

    Organize files and folders name etc

--- lib/app/features/home/pages/notes_page/notes_page.dart
+++ lib/app/features/home/pages/notes_page/notes_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:primary_school/app/features/home/pages/notes_page/add/add_note_page.dart';
 import 'package:primary_school/app/features/home/pages/notes_page/screens/general_notes/general_notes.dart';
 import 'package:primary_school/constans/colors.dart';
 import 'package:primary_school/constans/fonts_style.dart';
+import 'package:primary_school/features/features/home/pages/notes_page/screens/add_note_page/add_note_page.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({Key? key}) : super(key: key);
