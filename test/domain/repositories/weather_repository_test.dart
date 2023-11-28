b736d9e7792b9fb45e3b08dc975fd535645432c8
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-11-28 20:42:37 +0100

    Create MockWeatherDataSource

--- test/domain/repositories/weather_repository_test.dart
+++ test/domain/repositories/weather_repository_test.dart
@@ -1,3 +1,7 @@
-void main () {
-  
-}
+import 'package:mocktail/mocktail.dart';
+import 'package:primary_school/data/remote_data_sources_api/weather_remote_data_source.dart';
+
+class MockWeatherDataSource extends Mock
+    implements WeatherRemoteRetrofitDataSource {}
+
+void main() {}
