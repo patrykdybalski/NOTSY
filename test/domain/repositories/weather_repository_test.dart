985b6b20cde217c27a0987a2473795484d6ab3f0
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-12-11 20:08:20 +0100

    replacing hardcoded values with variables in WeatherRepoTest

--- test/domain/repositories/weather_repository_test.dart
+++ test/domain/repositories/weather_repository_test.dart
@@ -16,35 +16,32 @@ void main() {
     sut = WeatherRepository(weatherRemoteDataSource: dataSource);
   });
 
+  final testWeatherModel = WeatherModel(
+    Location('Warszawa', 'Poland'),
+    Current(10, Condition('Sunny'), 10, 'N', 1000, 10, 10, 10, 10),
+  );
+
+  const String testCity = 'Warszawa';
+
   group('getWeatherModel', () {
     test('should call weatherRemoteDataSource.getCurrentWeather', () async {
-      when(() => dataSource.getCurrentWeather('Warszawa'))
-          .thenAnswer((_) async => WeatherModel(
-                Location('Warszawa', 'Poland'),
-                Current(10, Condition('Sunny'), 10, 'N', 1000, 10, 10, 10, 10),
-              ));
-      //2
+      when(() => dataSource.getCurrentWeather(testCity))
+          .thenAnswer((_) async => testWeatherModel);
+
       await sut.getWeatherModel(city: 'Warszawa');
-      //3
-      verify(() => dataSource.getCurrentWeather('Warszawa')).called(1);
+
+      verify(() => dataSource.getCurrentWeather(testCity)).called(1);
     });
 
     test('should show weather data with provider city', () async {
-      //1
-      when(() => dataSource.getCurrentWeather('Warszawa'))
-          .thenAnswer((_) async => WeatherModel(
-                Location('Warszawa', 'Poland'),
-                Current(10, Condition('Sunny'), 10, 'N', 1000, 10, 10, 10, 10),
-              ));
-      //2
-      final result = await sut.getWeatherModel(city: 'Warszawa');
-      //3
+      when(() => dataSource.getCurrentWeather(testCity))
+          .thenAnswer((_) async => testWeatherModel);
+
+      final result = await sut.getWeatherModel(city: testCity);
+
       expect(
         result,
-        WeatherModel(
-          Location('Warszawa', 'Poland'),
-          Current(10, Condition('Sunny'), 10, 'N', 1000, 10, 10, 10, 10),
-        ),
+        testWeatherModel,
       );
     });
   });
