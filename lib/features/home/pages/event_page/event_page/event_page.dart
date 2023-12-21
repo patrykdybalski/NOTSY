7581c85598b1b633e40f0ddb56dd1c3040bf70f9
Patryk Dybalski <dybalski.patryk20@gmail.com>
2023-12-21 22:38:33 +0100

    Change places with Status in EventPage

--- lib/features/home/pages/event_page/event_page/event_page.dart
+++ lib/features/home/pages/event_page/event_page/event_page.dart
@@ -26,10 +26,8 @@ class _EventPageState extends State<EventPage> {
         centerTitle: true,
         backgroundColor: AppColors.primaryColor,
         forceMaterialTransparency: true,
-        title: const Text(
-          'Zaplanowane',
-        ),
         titleTextStyle: TextStyles.textStyle2(24),
+        title: const Text('Zaplanowane'),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: AppColors.greenLogoColor,
@@ -57,32 +55,28 @@ class _EventPageState extends State<EventPage> {
                   child: CircularProgressIndicator(),
                 );
 
-              case Status.success:
-                return ListView(
-                  children: [
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Column(
-                      children: [
-                        for (final eventModel in eventModels) ...[
-                          SlidableEventWidget(eventModel: eventModel),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                        ],
-                      ],
-                    ),
-                  ],
-                );
               case Status.error:
                 return Center(
                   child: Text(
-                    state.errorMessage ?? 'Unknown error' ,
+                    state.errorMessage ?? 'Unknown error',
                     style: const TextStyle(
                       color: Colors.red,
                     ),
                   ),
+                );
+              case Status.success:
+                return ListView(
+                  children: [
+                    const SizedBox(height: 15),
+                    Column(
+                      children: [
+                        for (final eventModel in eventModels) ...[
+                          SlidableEventWidget(eventModel: eventModel),
+                          const SizedBox(height: 10),
+                        ],
+                      ],
+                    ),
+                  ],
                 );
             }
           },
